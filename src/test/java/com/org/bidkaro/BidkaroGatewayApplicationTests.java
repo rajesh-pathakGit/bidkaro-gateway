5975cf4dc089f42de9a96b018c093b9f6c14db54
Rajesh Pathak <rrajeshpathak7@gmail.com>
2024-03-22 15:18:24 +0530

    first checkin

---
+++ src/test/java/com/org/bidkaro/BidkaroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bidkaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidkaroGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
